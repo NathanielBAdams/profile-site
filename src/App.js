59e6aa637882c1b3ee39765600781a44f716cb74
Nathaniel Adams <NathanielBAdams@gmail.com>
2021-10-30 13:41:09 -0400

    init basic styles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <div className='container'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm Nathaniel (but you can call me Nate). Sunt enim sint dolore
+          ullamco cupidatat do commodo nisi duis minim mollit deserunt ad. Dolor
+          dolore voluptate anim in esse. Excepteur minim ad aliquip aute commodo
+          qui laborum pariatur eu. Eu labore consectetur laborum proident amet.
+          Excepteur cillum ut excepteur est eu dolor dolor pariatur ea quis sit
+          et. Magna voluptate ullamco sit fugiat sunt consectetur aliqua.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
