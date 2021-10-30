7719d12a20c35e8a163b14f1534ce954c1d9f854
Nathaniel Adams <NathanielBAdams@gmail.com>
2021-10-30 17:31:59 -0400

    styling and content updates

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
+      <div class='footer'>@naaadams</div>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,23 @@
 export default function Home() {
   return (
-      <div className='container'>
-        <p>
-          I'm Nathaniel (but you can call me Nate). Sunt enim sint dolore
-          ullamco cupidatat do commodo nisi duis minim mollit deserunt ad. Dolor
-          dolore voluptate anim in esse. Excepteur minim ad aliquip aute commodo
-          qui laborum pariatur eu. Eu labore consectetur laborum proident amet.
-          Excepteur cillum ut excepteur est eu dolor dolor pariatur ea quis sit
-          et. Magna voluptate ullamco sit fugiat sunt consectetur aliqua.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </div>
+    <div className='container'>
+      <p>
+        I'm Nathaniel (but you can call me Nate). I'm originally from the
+        suburbs of Chicago and now live in New York.
+      </p>
+      <p>
+        <a href='/music' className='anti-link'>
+          I compose and publish music
+        </a>{' '}
+        for choirs and vocal ensembles.
+      </p>
+      <p>
+        <a href='/music' className='anti-link'>
+          I create and maintain software
+        </a>{' '}
+        for modern web applications.
+      </p>
+      <p>I also make pizza from scratch on a weekly basis.</p>
+    </div>
   );
 }
